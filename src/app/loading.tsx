fa20de6064ccb0bf7615b1fa5559853bae3bc03a
dorkele <dora_tominic@hotmail.com>
2023-09-25 14:59:16 +0200

    starting from scratch due to redesign

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./page.module.css";
-import CircularElement from "@/components/CircularElement/CircularElement";
-import WorkModal from "@/components/WorkModal/WorkModal";
-import Sidenav from "@/components/Sidenav/Sidenav";
-
-export default function Loading() {
-    return <main className={styles.main}>HAHAHAHAHAHAHA</main>;
-}
